3be7a80d9b4e82ea6c82928637e4022a87f1eb4e
Iggy <igoririanto@rocketmail.com>
2018-10-22 17:32:52 -0500

    update cms pageview and edit action controllers for page

--- lib/learn_context_web/views/cms/page_view.ex
+++ lib/learn_context_web/views/cms/page_view.ex
@@ -1,3 +1,9 @@
 defmodule LearnContextWeb.CMS.PageView do
   use LearnContextWeb, :view
+
+  alias LearnContext.CMS
+
+  def author_name(%CMS.Page{author: author}) do
+    author.user.name
+  end
 end
